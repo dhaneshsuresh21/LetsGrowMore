6da54853677710ee719563ff1533a378c800cf3c
dhaneshsuresh21 <75305982+dhaneshsuresh21@users.noreply.github.com>
2021-11-29 23:47:14 +0530

    Add files via upload

---
+++ Task_2/React_app/src/App.js
@@ -0,0 +1,16 @@
+import React,{Component} from 'react';
+import UseEffectApi from './Components/UseEffect/UseEffectApi';
+
+
+function App() {
+  
+  return (
+    <>
+    <div>
+      <UseEffectApi />
+    </div>
+    </>
+  )
+}
+
+export default App;
